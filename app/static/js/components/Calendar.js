14e4d499762a56d844860c5bd8a2e942319bf8b2
gadeta01 <gadeta01@luther.edu>
2017-10-27 10:11:57 -0500

    basic cal display set up

--- app/static/js/components/Calendar.js
+++ app/static/js/components/Calendar.js
@@ -14,40 +14,20 @@ class Calendar extends Component {
   }
 
   componentDidMount() {
-    var url =  origin + '/api/calendar'
-    axios.get(url)
-    .then(res => {
-      const results = res.data.calData
-      this.setState({ results });
-    });
+    // var url =  origin + '/api/calendar'
+    // axios.get(url)
+    // .then(res => {
+    //   const results = res.data.calData
+    //   this.setState({ results });
+    // });
       
   }  
 
   render() {
-    var myEventsList = {
-      events: [
-          {
-              title: 'Event1',
-              start: '2017-04-04'
-          },
-          {
-              title: 'Event2',
-              start: '2017-05-05'
-          }
-          // etc...
-      ],
-      color: 'yellow',   // an option!
-      textColor: 'black' // an option!
-    };
 
     return ( 
       <div className="Calendar">
-        <h3> Your calendar! </h3> 
-        <h1></h1>
-        <ul>
-            <li key={this.state.results}>{this.state.results}</li>
-        </ul>
-        <MyCalendar myEvents={myEventsList}/>
+        <MyCalendar /> 
       </div>
     );
   }
--- app/static/js/components/MyCalendar.js
+++ app/static/js/components/MyCalendar.js
@@ -2,22 +2,102 @@
 import React, { Component } from 'react';
 import BigCalendar from 'react-big-calendar'; 
 import moment from 'moment'; 
+import 'react-big-calendar/lib/css/react-big-calendar.css'; 
 
-BigCalendar.momentLocalizer(moment);
+BigCalendar.setLocalizer(
+  BigCalendar.momentLocalizer(moment)
+);
+
+const myevents = [{
+  title: 'First Shift',
+  start:  new Date(2017, 9, 25, 7, 30),
+  end: new Date(2017, 9, 25, 9),
+},
+{
+  title: 'First Shift',
+  start:  new Date(2017, 9, 25, 7, 30),
+  end: new Date(2017, 9, 25, 9, 30),
+},
+{
+  title: 'First Shift',
+  start:  new Date(2017, 9, 25, 9),
+  end: new Date(2017, 9, 25, 10, 30),
+},
+{
+  title: 'First Shift',
+  start:  new Date(2017, 9, 25, 10, 30),
+  end: new Date(2017, 9, 25, 12, 15),
+},
+{
+  title: 'Hidden Shift',
+  start:  new Date(2017, 9, 26, 9, 15),
+  end: new Date(2017, 9, 26, 11),
+},
+{
+  title: 'Hidden Shift',
+  start:  new Date(2017, 9, 26, 9, 11),
+  end: new Date(2017, 9, 26, 12, 15),
+},
+{
+  title: 'Hidden Shift',
+  start:  new Date(2017, 9, 26, 12, 16),
+  end: new Date(2017, 9, 26, 1, 30),
+},
+{
+  title: 'Second Shift',
+  start:  new Date(2017, 9, 26, 9),
+  end: new Date(2017, 9, 26, 12),
+},
+{
+  title: 'Second Shift',
+  start:  new Date(2017, 9, 26, 9),
+  end: new Date(2017, 9, 26, 12),
+},
+{
+  title: 'Third Shift',
+  start:  new Date(2017, 9, 26, 9, 15),
+  end: new Date(2017, 9, 26, 11),
+}]
 
 class MyCalendar extends Component {
-  render() {
-     
+  constructor() {
+    super(); 
+    let timeRangeFormat = ({ start, end }, culture, local)=>
+      local.format(start, 'h:mm', culture) +
+      '-' + local.format(end,  'h:mm', culture)
+    let formats = {
+      dayFormat:'ddd MM/DD',
+      eventTimeRangeFormat: timeRangeFormat
+    }; 
+    let scrollToTime = new Date().setHours(7); 
+    this.state = {
+      format: formats,
+      scrollTime : scrollToTime
+    };
+  }  
 
+  render() {
+    const now = moment(); 
     return (
-      <div>
-      <BigCalendar
-        selectable
-        events={this.props.myEvents.events}
-        startAccessor='startDate'
-        endAccessor='endDate'
+      <div height="100px">
+        <BigCalendar
+          selectable
+          defaultView='week'
+          popup='true'
+          selectable
+          events={myevents}
+          style={{height: 500}}
+          formats={this.state.format}
+          scrollToTime={this.state.scrollTime}
+          onSelectEvent={event => alert(event.title)}
+          onSelectSlot={(slotInfo) => alert(
+            `selected slot: \n\nstart ${slotInfo.start.toLocaleString()} ` +
+            `\nend: ${slotInfo.end.toLocaleString()}`
+          )}
         />
       </div>
     )
   }
 }
+
+export default MyCalendar; 
