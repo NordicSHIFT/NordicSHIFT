0e5f53736d2a55f8342d5df643f77cb1efb534a7
gadeta01 <gadeta01@luther.edu>
2017-10-27 22:34:05 -0500

    create events by select

--- app/server/app.py
+++ app/server/app.py
@@ -1,6 +1,7 @@
 # server.py
-from flask import Flask, render_template, jsonify
+from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify
 from calRetrieve import *
+import datetime
 
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
 
@@ -26,8 +27,28 @@ def catch_all(path):
 #naming standard, if it is being used for an axios call, use /api/name_of_call 
 @app.route("/api/calendar")
 def calendar():
+  myevents = [{
+  "title": 'Python Event',
+  "start":  datetime.datetime(2017, 10, 25, 13),
+  "end": datetime.datetime(2017, 10, 25, 15),
+  "hexColor" : "#ee5f5b"
+  }]
+  #color scheme colors: #62c462, #5bc0de, #f89406,  #ee5f5b
   print("In calendar!!")
-  data = {"calData": "Calendar Data"}
+  data = {"calData": "Calendar Data", "events": myevents}
+  return jsonify(data)
+
+@app.route("/api/addEvent", methods = ['POST'])
+def addEvent():
+  print ('IN ADD EVENT!')
+  #title = request.args.get("title")
+  #print("title: ", title)
+  data = request.get_json(silent=True)
+  myEvent = data.get('myEvent')
+  myEvent['title'] = 'From Py title' 
+  myEvent['hexColor'] = '#f89406'
+  print('myEvent: ', myEvent)
+  data = myEvent
   return jsonify(data)
 
 if __name__ == "__main__":
--- app/static/js/components/MyCalendar.js
+++ app/static/js/components/MyCalendar.js
@@ -1,80 +1,130 @@
 //MyCalendar.js
 import React, { Component } from 'react';
 import BigCalendar from 'react-big-calendar'; 
-import moment from 'moment'; 
+import axios from 'axios';
+import moment from 'moment';  
 import 'react-big-calendar/lib/css/react-big-calendar.css'; 
 
 BigCalendar.setLocalizer(
   BigCalendar.momentLocalizer(moment)
 );
+var origin = window.location.origin;
 
-const myevents = [{
-  title: 'First Shift',
-  start:  new Date(2017, 9, 25, 7, 30),
-  end: new Date(2017, 9, 25, 9),
-},
-{
-  title: 'First Shift',
-  start:  new Date(2017, 9, 25, 7, 30),
-  end: new Date(2017, 9, 25, 9, 30),
-},
-{
-  title: 'First Shift',
-  start:  new Date(2017, 9, 25, 9),
-  end: new Date(2017, 9, 25, 10, 30),
-},
-{
-  title: 'First Shift',
-  start:  new Date(2017, 9, 25, 10, 30),
-  end: new Date(2017, 9, 25, 12, 15),
-},
-{
-  title: 'Hidden Shift',
-  start:  new Date(2017, 9, 26, 9, 15),
-  end: new Date(2017, 9, 26, 11),
-},
-{
-  title: 'Hidden Shift',
-  start:  new Date(2017, 9, 26, 9, 11),
-  end: new Date(2017, 9, 26, 12, 15),
-},
-{
-  title: 'Hidden Shift',
-  start:  new Date(2017, 9, 26, 12, 16),
-  end: new Date(2017, 9, 26, 1, 30),
-},
-{
-  title: 'Second Shift',
-  start:  new Date(2017, 9, 26, 9),
-  end: new Date(2017, 9, 26, 12),
-},
-{
-  title: 'Second Shift',
-  start:  new Date(2017, 9, 26, 9),
-  end: new Date(2017, 9, 26, 12),
-},
-{
-  title: 'Third Shift',
-  start:  new Date(2017, 9, 26, 9, 15),
-  end: new Date(2017, 9, 26, 11),
-}]
+function convertEvent(event) {
+  return {
+    title: event.title,
+    start: new Date(event.start),
+    end: new Date(event.end),
+    hexColor: event.hexColor
+  }
+}
+
+function postEventHelper (eventInfo) {
+  var eventResponse = postEvent(eventInfo); 
+  console.log("eventResponse", eventResponse); 
+  // return axios.all([postEvent(eventInfo)])
+  // .then(function(arr){
+  //   console.log("ARRG", arr); 
+  //   return {
+  //     title: arr[0].data.eventTitle,
+  //   }
+  // })
+
+  
+}
 
 class MyCalendar extends Component {
   constructor() {
-    super(); 
+    super();
+    
     let timeRangeFormat = ({ start, end }, culture, local)=>
-      local.format(start, 'h:mm', culture) +
+    local.format(start, 'h:mm', culture) +
       '-' + local.format(end,  'h:mm', culture)
+    let dayFormat = 'ddd MM/DD'; 
     let formats = {
-      dayFormat:'ddd MM/DD',
+      dayFormat:dayFormat,
       eventTimeRangeFormat: timeRangeFormat
     }; 
+
     let scrollToTime = new Date().setHours(7); 
+
+    this.retrieveEvents();  
     this.state = {
       format: formats,
-      scrollTime : scrollToTime
+      scrollTime : scrollToTime,
+      events: [{}]
     };
+
+    this.retrieveEvents = this.retrieveEvents.bind(this);
+    this.addEvent = this.addEvent.bind(this); 
+    this.eventStyleGetter = this.eventStyleGetter.bind(this); 
   }  
+
+  retrieveEvents() {
+    console.log("Before Axios call"); 
+    var url =  origin + '/api/calendar';  
+    axios.get(url)
+    .then(res => {
+      var jsevents = res.data.events.map(
+                        event => convertEvent(event)); 
+      var newevents = this.state.events.concat(jsevents); 
+      this.setState({ events: newevents }); 
+    })
+    .catch(function (error) {
+      console.log(error);
+    });  
+  }
+
+  addEvent(slotInfo) {
+    // alert(
+    //   `selected slot: \n\nstart ${slotInfo.start.toLocaleString()} ` +
+    //   `\nend: ${slotInfo.end.toLocaleString()}` + ` ${slotInfo.action} `
+    // );  
+    if (slotInfo.action == 'select') { 
+      var config = { headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'}
+      }
+      var respData = axios.post('/api/addEvent', {
+        myEvent: slotInfo
+      }, config) 
+      .then( (response) => {
+        const responseData = response.data 
+        console.log(responseData); 
+        const theEvent = {
+          title: responseData.title,
+          hexColor: responseData.hexColor, 
+          start: new Date(responseData.start),
+          end: new Date(responseData.end),
+        }; 
+        var theevents = this.state.events.concat([theEvent]);
+        this.setState({events: theevents}); 
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+      
+      //TODO add interaction with backend, to save event to the db  
+      //TODO possibly a pop-up form to enter in other details of shift 
+      //TODO ask if you want to create the shift or not  
+    }
+  }
+
+  eventStyleGetter(event) {
+    console.log(event);
+    var backgroundColor = event.hexColor;
+    var style = {
+        backgroundColor: backgroundColor,
+        borderRadius: '0px',
+        opacity: 0.8,
+        color: 'black',
+        border: '0px',
+        display: 'block'
+    };
+    return {
+        style: style
+    };
+  }
 
   render() {
     const now = moment(); 
@@ -84,17 +134,15 @@ class MyCalendar extends Component {
           selectable
           defaultView='week'
           popup='true'
-          selectable
-          events={myevents}
+          events={this.state.events}
           style={{height: 500}}
           formats={this.state.format}
           scrollToTime={this.state.scrollTime}
-          onSelectEvent={event => alert(event.title)}
-          onSelectSlot={(slotInfo) => alert(
-            `selected slot: \n\nstart ${slotInfo.start.toLocaleString()} ` +
-            `\nend: ${slotInfo.end.toLocaleString()}`
-          )}
+          onSelectEvent={event => alert(event.title + event.hexColor)}
+          onSelectSlot={(slotInfo) => this.addEvent(slotInfo)}
+          eventPropGetter={(this.eventStyleGetter)}
         />
+        <p>{this.state.results}</p>
       </div>
     )
   }
