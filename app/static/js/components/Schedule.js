b3707a6711191c2c11c30ec85de808bbd7e847ce
gadeta01 <gadeta01@luther.edu>
2017-10-29 16:38:29 -0500

    student version of calendar

--- app/static/js/components/Schedule.js
+++ app/static/js/components/Schedule.js
@@ -1,11 +1,13 @@
 // Schedule.js
 import React, { Component } from 'react';
+import StudentCal from './StudentCal'; 
 
 class Schedule extends Component {
   render() {
     return ( 
       <div className="Schedule">
         <h3> Schedule! </h3> 
+        <StudentCal /> 
       </div>
     );
   }
