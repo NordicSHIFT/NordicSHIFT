fc3a51cd3ed49a99270c0cfae166635c1f6f90ca
ianchrist <iechrist14@gmail.com>
2017-10-29 19:55:52 -0500

    Attempt to fix heroku

--- start.py
+++ start.py
@@ -1,13 +1,5 @@
 import subprocess
 
-subprocess.check_call('npm install',
-                      cwd='./app/static',
-                      shell=True)
-
-subprocess.check_call('npm run build',
-                      cwd='./app/static',
-                      shell=True)
-
 subprocess.check_call('python3 app.py',
                        cwd='./app/server/',
                        shell=True)
