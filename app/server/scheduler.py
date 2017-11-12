f37a4f213852362ae550ebc64dff992533312eb5
ianchrist <iechrist14@gmail.com>
2017-11-12 16:40:48 -0600

    Scheduler skeleton

---
+++ app/server/scheduler.py
@@ -0,0 +1,13 @@
+class Scheduler:
+    def __init__():
+        self.bad_schedules = []
+        self.good_schedules = []
+    
+    def getBadSchedules():
+        return self.bad_schedules
+
+    def getGoodSchedules():
+        return self.good_schedules
+
+    def scheduler(shifts):
+        pass
