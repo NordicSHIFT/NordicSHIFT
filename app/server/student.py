0382ee5a8f5ec4de6f2888834f3a46e45456c060
Linh Nguyen Le <nguyli03@.luther.edu>
2017-11-12 17:21:35 -0600

    add some sample shift and some methods for student class

--- app/server/student.py
+++ app/server/student.py
@@ -9,15 +9,27 @@ class Student:
         self.hours = hours
         self.unavailability = unavailability
         self.shiftAssigned = shiftAssigned
-    
+
     def getUsername():
         return self.username
 
     def getHours():
         return self.hours
 
+    # unavailability is a list of tuple with start time and end time of when student is unavailable
     def getUnavailability():
         return self.unavailability
 
     def getShiftAssigned():
         return self.shiftAssigned
+
+    def isAvailable(time): #time passed in should be in a format of a tuple with start and end timeslots
+        return not time in self.unavailable
+
+    def assignShift(shift):
+        self.hours -= shift.getLength()
+        self.shiftAssigned.append(shift)
+
+    def removeFromShift(shift):
+        self.hours += shift.getLength()
+        self.shiftAssigned.remove(shift)
