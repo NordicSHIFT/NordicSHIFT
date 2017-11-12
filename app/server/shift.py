0c5219f45cd912eecb36310d445754e766d238d2
ianchrist <iechrist14@gmail.com>
2017-11-12 16:37:01 -0600

    Skeleton code

---
+++ app/server/shift.py
@@ -0,0 +1,18 @@
+class Shift:
+    def __init__(dept, student, start, end):
+        self.dept = dept
+        self.student = student
+        self.start = start
+        self.end = end
+
+    def getDept():
+        return self.dept
+
+    def getStudent():
+        return self.student
+
+    def getStart():
+        return self.start
+
+    def getEnd():
+        return self.end
---
+++ app/server/student.py
@@ -0,0 +1,18 @@
+class Student:
+    def __init__(self, username, hours, unavailability, shiftAssigned):
+        self.username = username
+        self.hours = hours
+        self.unavailability = unavailability
+        self.shiftAssigned = shiftAssigned
+    
+    def getUsername():
+        return self.username
+
+    def getHours():
+        return self.hours
+
+    def getUnavailability():
+        return self.unavailability
+
+    def getShiftAssigned():
+        return self.shiftAssigned
