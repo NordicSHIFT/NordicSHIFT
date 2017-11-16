c401ef8af8898db3b2dbb98259e5104eb5a04471
gadeta01 <gadeta01@luther.edu>
2017-11-16 15:42:20 -0600

    resolve merge conflict

--- app/server/scheduler.py
+++ app/server/scheduler.py
@@ -77,47 +77,9 @@ class Scheduler:
     def getAssignedShift(self):
         return self.assignedShift
 
-<<<<<<< HEAD
     def __eq__(self, other):
         if self.assignedShift == other.getAssignedShift()
-=======
-    def scheduler(self, shifts, students): #where it all goes down
-        while (len(self.good_schedules) < 3):
-          self.available_shifts = shifts
-          while len(self.available_shifts) != 0:
-            shift = self.available_shifts.pop()
-            i = 0
-            while shift.getStudent() == None and i < len(students):
-              student = students[i]
-              if student.isAvailable(shift):
-                #add student to shift
-                shift.setStudent(student)
-                student.assignShift(shift)
-                self.assigned_shifts.append(shift)
-                if (self.assigned_shifts in self.bad_schedules or self.assigned_shifts in self.good_schedules):
-                  '''This schedule has already been tried, so we want to remove the shift we
-                    just assigned, and then look for another student
-                  '''
-                  self.assigned_shifts.remove(-1)
-                  student.removeFromShift(shift)
-                  shift.setStudent(None)
-                  i += 1
-              else:
-                '''that student was not availabel, so we move onto the next'''
-                i += 1
 
-            if shift.getStudent() == None:
-              '''we could not find an available student, the schedule we attempted won't work,
-              we add it to the bad schedules, and remove the last assigned shift '''
-              self.bad_schedules.append(self.assigned_shifts)
-              lastAssigned = self.assigned_shifts.pop()
-              student.removeFromShift(lastAssigned)
-              lastAssigned.setStudent(None)
-              self.available_shifts.append(lastAssigned)
-          '''if we make it here, every shift has been assigned a student, we add the schedule
-          to the list of good schedules'''
-          self.good_schedules.append(self.assigned_shifts)
->>>>>>> 9e604a72d8b341e97867823f6b70584af22361e8
 
 def main():
     res = db.execute("""SELECT * from shift;""")
