62df6ab0e60c6b3b1c9298e1bc506a8e254d7747
Ian Christopherson <iechrist14@gmail.com>
2018-03-06 16:03:17 -0600

    Email (#46)

---
+++ app/server/auto_email.py
@@ -0,0 +1,30 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def sendEmail(recipients, subject, message_body):
+    FROMADDR = EMAIL_NAME
+    PASSWORD = EMAIL_PASS
+    
+    msg = MIMEMultipart()
+    msg['From'] = FROMADDR
+    msg['To'] = ", ".join(recipients)
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(message_body, 'plain'))
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(FROMADDR, PASSWORD)
+    text = msg.as_string()
+    server.sendmail(FROMADDR, recipients, msg.as_string())
+    server.quit()
+
+def published_sched_notif(recipients):
+    SUBJECT = "Upcoming schedule has been published"
+    MESSAGE = "Management has posted the upcoming schedule.\n" +
+              "Please login to your account to view the full details.\n" +
+              "https://nordicshift.heroku.com/\n" +
+              "Thank you."
+
+    sendEmail(recipients, SUBJECT, MESSAGE)
