1abd02d03f4c43a58734c002f397fa96db6d6e6e
ianchrist <iechrist14@gmail.com>
2018-03-07 13:04:53 -0600

    Email bug fix

--- app/server/auto_email.py
+++ app/server/auto_email.py
@@ -22,9 +22,9 @@ def sendEmail(recipients, subject, message_body):
 
 def published_sched_notif(recipients):
     SUBJECT = "Upcoming schedule has been published"
-    MESSAGE = "Management has posted the upcoming schedule.\n" +
-              "Please login to your account to view the full details.\n" +
-              "https://nordicshift.heroku.com/\n" +
+    MESSAGE = "Management has posted the upcoming schedule.\n" + \
+              "Please login to your account to view the full details.\n" + \
+              "https://nordicshift.heroku.com/\n" + \
               "Thank you."
 
     sendEmail(recipients, SUBJECT, MESSAGE)
