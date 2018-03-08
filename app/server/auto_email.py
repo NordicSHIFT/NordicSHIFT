e7ca6b38cb636cd797b668146851896cd0f1c54b
ianchrist <iechrist14@gmail.com>
2018-03-08 15:13:56 -0600

    Cleanup

--- app/server/auto_email.py
+++ app/server/auto_email.py
@@ -1,14 +1,22 @@
+'''
+Allows NordicShift@gmail.com to send emails.
+'''
 import smtplib
 import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-def sendEmail(recipients, subject, message_body):
-    FROMADDR = os.environ['EMAIL_NAME']
-    PASSWORD = os.environ['EMAIL_PASS']
-    
+def send_email(recipients, subject, message_body):
+    """ Sends out an email with the subject and message
+    provided to the recipients provided. Recipients may
+    take form as a string or a list. The emails will be
+    send through NordicShift@gmail.com
+    """
+    fromaddr = os.environ['EMAIL_NAME']
+    password = os.environ['EMAIL_PASS']
+
     msg = MIMEMultipart()
-    msg['From'] = FROMADDR
+    msg['From'] = fromaddr
     msg['To'] = ", ".join(recipients)
     msg['Subject'] = subject
 
@@ -16,16 +24,22 @@ def sendEmail(recipients, subject, message_body):
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
-    server.login(FROMADDR, PASSWORD)
-    text = msg.as_string()
-    server.sendmail(FROMADDR, recipients, msg.as_string())
+    server.login(fromaddr, password)
+    server.sendmail(fromaddr, recipients, msg.as_string())
     server.quit()
 
 def published_sched_notif(recipients):
-    SUBJECT = "Upcoming schedule has been published"
-    MESSAGE = "Management has posted the upcoming schedule.\n" + \
+    """ Sends out an email telling the recipients that
+    they have been scheduled to work. Recipients may
+    be provided as a string or an email. The email is
+    sent through NordicShift@gmail.com
+
+    """
+
+    subject = "Upcoming schedule has been published"
+    message = "Management has posted the upcoming schedule.\n" + \
               "Please login to your account to view the full details.\n" + \
               "https://nordicshift.heroku.com/\n" + \
               "Thank you."
 
-    sendEmail(recipients, SUBJECT, MESSAGE)
+    send_email(recipients, subject, message)
