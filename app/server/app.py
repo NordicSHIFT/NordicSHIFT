2bb25bec9d6f10effe2bfb49ec87b60972aecfd7
Ian Christopherson <iechrist14@gmail.com>
2018-03-13 15:09:50 -0500

    Auto email (#51)
    * Automatic email notification implemented
    
    * Disable email by default
    
    * Cleanup
    
    * Auto email attempt...
    
    * Emails implemented

--- app/server/app.py
+++ app/server/app.py
@@ -3,6 +3,7 @@ from flask import Flask, flash, redirect, render_template, request, session, abo
 from oAuth import *
 import datetime
 import os
+import auto_email
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Table, Column, Integer, String, create_engine, Sequence, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship
@@ -11,6 +12,8 @@ import hashlib, uuid
 from scheduler import scheduler2, Schedule
 from shift import Shift
 from student import Student
+
+SEND_EMAILS = False
 
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
 app.secret_key=os.environ['SECRET_KEY']
@@ -407,14 +410,23 @@ def generateSchedule():
 
 @app.route('/api/chooseSchedule', methods=['POST'])
 def chooseSchedule():
+    workers = []
+
     print ("IN CHOOSE SCHEDULE")
     data = request.get_json(silent=True)
     schedule = data.get('schedule')['assigned shifts']
+    email = data.get('email')
     # print("schedule", schedule)
     # This function will save the selected schedule in the database by making changes to the shift schedule with student for each shift
     for shift in schedule:
+        if email == True:
+            cur_worker = shift['student']
+            if cur_worker not in workers:
+                workers.append(cur_worker)
         db.execute("UPDATE shift SET student = (select id from student where username = '%s') where id = '%d';"%(shift['student'],shift['id']))
         db.commit()
+    if email == True:
+        auto_email.published_sched_notif(workers)
     return "done"
 
 @app.route('/api/retrieveSchedule', methods=['GET','OPTIONS'])
