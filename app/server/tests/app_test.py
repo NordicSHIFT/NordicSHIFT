6ad60fa31e2536311e7b0faff09f8b7e301ea87c
ianchrist <iechrist14@gmail.com>
2018-03-22 13:59:15 -0500

    testssss

--- app/server/tests/app_test.py
+++ app/server/tests/app_test.py
@@ -7,6 +7,11 @@ import unittest
 import tempfile
 
 class FlaskTestCase(unittest.TestCase):
+
+    ######################
+    # SetUp and TearDown #
+    ######################
+
     def setUp(self):
         self.db_fd, ns.app.config['DATABASE'] = tempfile.mkstemp()
         ns.app.testing = True
@@ -18,9 +23,34 @@ class FlaskTestCase(unittest.TestCase):
         os.close(self.db_fd)
         os.unlink(ns.app.config['DATABASE'])
 
+    ##################
+    # Helper methods #
+    ##################
+
+    def login(self, username, password):
+        return self.app.post(
+            '/loginC',
+            data=dict(username=username, password=password),
+            follow_redirects=True
+        )
+
+    def logout(self):
+        return self.app.get(
+            '/logout',
+            follow_redirects=True
+        )
+
+    #########
+    # Tests #
+    #########
+
     def test_home_status_code(self):
         result = self.app.get('/')
         self.assertEqual(result.status_code, 200)
+
+    def test_redir_to_login(self):
+        result = self.app.get('/managerprofile')
+        self.assertEqual(result.status_code, 302)
 
 if __name__ == "__main__":
     unittest.main()
