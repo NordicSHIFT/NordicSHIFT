fb7bfc3b1c90b1663aff1feb5660295688df28ab
gadeta01 <gadeta01@luther.edu>
2018-03-22 15:43:06 -0500

    add goat

--- app/static/js/components/ErrorPage.js
+++ app/static/js/components/ErrorPage.js
@@ -4,8 +4,9 @@ class ErrorPage extends Component {
   render() {
     return (
       <div>
-        <h1>There is an error!</h1>
+        <h1>Error!</h1>
         <p>You are trying to access a page that does not exists or you do not have access to </p>
+        <img src={require("./../../img/goat1.png")} alt="Goat Image" />
       </div>
     );
   }
